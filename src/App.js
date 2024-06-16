4f7c7c4c0d9397adf205c9fb5318e9b0e59910c1
Yash Thakare <thakareyash74@gmail.com>
2024-06-17 01:35:22 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './index.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Shop from './pages/Shop';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignup from './pages/LoginSignup';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop/>} />
+          <Route path='/mens' element={<ShopCategory category="men"/>}  />
+          <Route path='/womens' element={<ShopCategory category="women"/>} />
+          <Route path='/kids' element={<ShopCategory category="kids"/>} />
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>} />
+          </Route>
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/login' element={<LoginSignup/>} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React,{useState} from 'react';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    const [cartCount, setCartCount] = useState(0);
     const [menu,setMenu] = useState("Shop");
   return (
    <>
@@ -13,19 +13,14 @@ const Navbar = () => {
             <p className="text-xl font-bold">QuickCart</p>
         </div>
         <ul className="flex space-x-6">
-          <li className={`cursor-pointer ${menu === "shop" ? 'scale-110 text-slate-900 text-xl' : 'hover:scale-110 hover:text-slate-900 hover:text-xl '}`} onClick={()=>{setMenu("shop")}}>Shop</li>
-          <li className={`cursor-pointer ${menu === "men" ? 'scale-110 text-slate-900 text-xl' : 'hover:scale-110 hover:text-slate-900 hover:text-xl '}`}  onClick={()=>{setMenu("men")}}>Men</li>
-          <li className={`cursor-pointer ${menu === "women" ? 'scale-110 text-slate-900 text-xl' : 'hover:scale-110 hover:text-slate-900 hover:text-xl '}`}  onClick={()=>{setMenu("women")}}>Women</li>
-          <li className={`cursor-pointer ${menu === "kids" ? 'scale-110 text-slate-900 text-xl' : 'hover:scale-110 hover:text-slate-900 hover:text-xl '}`}  onClick={()=>{setMenu("kids")}}>Kids</li>
+          <li className={`cursor-pointer ${menu === "shop" ? 'scale-110 text-slate-900 text-xl' : 'hover:scale-110 hover:text-slate-900 hover:text-xl '}`} onClick={()=>{setMenu("shop")}}><Link to='/'> Shop</Link></li>
+          <li className={`cursor-pointer ${menu === "men" ? 'scale-110 text-slate-900 text-xl' : 'hover:scale-110 hover:text-slate-900 hover:text-xl '}`}  onClick={()=>{setMenu("men")}}><Link to='/men'> Men</Link></li>
+          <li className={`cursor-pointer ${menu === "women" ? 'scale-110 text-slate-900 text-xl' : 'hover:scale-110 hover:text-slate-900 hover:text-xl '}`}  onClick={()=>{setMenu("women")}}><Link to='/women'> Women</Link></li>
+          <li className={`cursor-pointer ${menu === "kids" ? 'scale-110 text-slate-900 text-xl' : 'hover:scale-110 hover:text-slate-900 hover:text-xl '}`}  onClick={()=>{setMenu("kids")}}><Link to='/kids'> Kids</Link></li>
         </ul>
         <div className="flex items-center space-x-4">
             <button className="px-6 py-2 bg-white  border-spacing-3 border-2 border-slate-300 rounded-3xl ">Login</button>
             <img src={cart_icon} alt="Cart" className="h-6" />
-            {cartCount > 0 && (
-              <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
-                {cartCount}
-              </span>
-            )}
         </div>
     </div>
      <hr className="border-t-2 border-gray-200 mt-2" />
